7172edcbe201d226843765f924a576772ea42b0a
Yamagn <syankenpon@gmail.com>
2020-01-08 17:45:31 +0900

    Initial Commit

---
+++ GHclient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GHclient
+//
+//  Created by ymgn on 2020/01/08.
+//  Copyright © 2020 ymgn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
