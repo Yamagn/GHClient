37e636ab8418a87be3da9a718981f5fd7d3764ba
Yamagn <syankenpon@gmail.com>
2020-01-09 13:58:00 +0900

    ViewModelの追加

---
+++ GHclient/Scenes/Search/SearchViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  SearchViewModel.swift
+//  GHClient
+//
+//  Created by ymgn on 2020/01/09.
+//  Copyright © 2020 ymgn. All rights reserved.
+//
+
+import Foundation
---
+++ GHclient/Scenes/User/UserViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  UserViewModel.swift
+//  GHClient
+//
+//  Created by ymgn on 2020/01/09.
+//  Copyright © 2020 ymgn. All rights reserved.
+//
+
+import Foundation
