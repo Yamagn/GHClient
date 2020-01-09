d11b8cc09a3d23cef20a4523f57423b8fe69e793
Yamagn <syankenpon@gmail.com>
2020-01-09 18:52:35 +0900

    GetUsersRequestの追加

---
+++ GHclient/Models/Responses.swift
@@ -0,0 +1,37 @@
+//
+//  Responses.swift
+//  GHClient
+//
+//  Created by ymgn on 2020/01/09.
+//  Copyright © 2020 ymgn. All rights reserved.
+//
+
+import Foundation
+
+public struct GetUsersResponse: Codable {
+    let totalCount: Int
+    let incompleteResults: Bool
+    let items: [UserItem]
+    
+    enum CodingKeys: String, CodingKey {
+        case totalCount = "total_count"
+        case incompleteResults = "incomplete_results"
+        case items
+    }
+}
+
+public struct UserItem: Codable {
+    let login: String
+    let id: Int
+    let avatarUrl: String
+    let reposUrl: String
+    let type: String
+    
+    enum CodingKeys: String, CodingKey {
+        case login
+        case id
+        case avatarUrl = "avatar_url"
+        case reposUrl = "repos_url"
+        case type
+    }
+}
--- GHclient/Network/API.swift
+++ GHclient/Network/API.swift
@@ -7,3 +7,49 @@
 //
 
 import Foundation
+import APIKit
+
+final class DecodableDataParser: DataParser {
+    var contentType: String? {
+        return "applicaiton/json"
+    }
+    
+    func parse(data: Data) throws -> Any {
+        return data
+    }
+}
+
+protocol GitHubRequest: Request {}
+
+extension GitHubRequest {
+    public var baseURL: URL {
+        return URL(string: "https://api.github.com/")!
+    }
+}
+
+public struct GetUsers: GitHubRequest {
+    public var dataParser: DataParser {
+        return DecodableDataParser()
+    }
+    
+    public typealias Response = GetUsersResponse
+    public let method: HTTPMethod = .get
+    public var path: String {
+        return "/search/users"
+    }
+    
+    public var parameters: Any? {
+        return ["q": q, "sort": sort, "order": order]
+    }
+    
+    let q: String
+    let sort: String
+    let order: String
+    
+    public func response(from object: Any, urlResponse: HTTPURLResponse) throws -> GetUsersResponse {
+        guard let data = object as? Data else {
+            throw ResponseError.unexpectedObject(object)
+        }
+        return try JSONDecoder().decode(GetUsersResponse.self, from: data)
+    }
+}
