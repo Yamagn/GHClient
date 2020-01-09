7bea8a00afc8ca3e4678946a29fb336b1c091fed
Yamagn <syankenpon@gmail.com>
2020-01-09 13:12:37 +0900

    ファイルの追加

---
+++ GHclient/Network/API.swift
@@ -0,0 +1,9 @@
+//
+//  API.swift
+//  GHClient
+//
+//  Created by ymgn on 2020/01/09.
+//  Copyright © 2020 ymgn. All rights reserved.
+//
+
+import Foundation
---
+++ GHclient/Scenes/Auth/AuthViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AuthViewController.swift
+//  GHClient
+//
+//  Created by ymgn on 2020/01/09.
+//  Copyright © 2020 ymgn. All rights reserved.
+//
+
+import UIKit
+
+class AuthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
