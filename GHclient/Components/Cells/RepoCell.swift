dfd32697e09d255c2b5943124199bd156eae3320
Yamagn <syankenpon@gmail.com>
2020-01-12 15:08:42 +0900

    Viewの追加

---
+++ GHclient/Components/Cells/RepoCell.swift
@@ -0,0 +1,24 @@
+//
+//  RepoCell.swift
+//  GHClient
+//
+//  Created by ymgn on 2020/01/12.
+//  Copyright © 2020 ymgn. All rights reserved.
+//
+
+import UIKit
+
+class RepoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
