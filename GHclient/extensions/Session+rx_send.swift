0f2a012b826809befd1d4bd633fa6601dfeba541
Yamagn <syankenpon@gmail.com>
2020-01-12 12:34:53 +0900

    RxSwiftをAPIKitをつなげるExtensionを追加

--- GHclient/extensions/Session+rx_send.swift
+++ GHclient/extensions/Session+rx_send.swift
@@ -6,4 +6,28 @@
 //  Copyright © 2020 ymgn. All rights reserved.
 //
 
-import Foundation
+import APIKit
+import RxSwift
+
+extension Session {
+    func rx_send<T: Request>(request: T) -> Observable<T.Response> {
+        return Observable.create { observer in
+            let task = self.send(request) { result in
+                switch result {
+                    case .success(let res):
+                        observer.on(.next(res))
+                        observer.on(.completed)
+                    case .failure(let error):
+                        observer.on(.error(error))
+                }
+            }
+            return Disposables.create {
+                task?.cancel()
+            }
+        }
+    }
+    
+    class func rx_send<T: Request>(request: T) -> Observable<T.Response> {
+        return shared.rx_send(request: request)
+    }
+}
