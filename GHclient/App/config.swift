48755342d179019ba608f784d82369586a82f017
Yamagn <syankenpon@gmail.com>
2020-01-19 16:33:22 +0900

    OAuthSwiftの導入

--- GHclient/App/config.swift
+++ GHclient/App/config.swift
@@ -1,8 +1,8 @@
 //
-//  AuthViewModel.swift
-//  GHClient
+//  config.swift
+//  GHclient
 //
-//  Created by ymgn on 2020/01/09.
+//  Created by ymgn on 2020/01/18.
 //  Copyright © 2020 ymgn. All rights reserved.
 //
 
