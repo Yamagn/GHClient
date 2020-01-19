6fb0ace48e1e816d711ebfb2f4f5759e51006148
Yamagn <syankenpon@gmail.com>
2020-01-19 16:36:07 +0900

    OAuthSiwftの操作を反映させる

--- GHclient/SceneDelegate.swift
+++ GHclient/SceneDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import OAuthSwift
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -48,6 +49,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // to restore the scene back to its current state.
     }
 
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+            guard let url = URLContexts.first?.url else {
+                return
+            }
+            if (url.host == "oauth-callback") {
+                OAuthSwift.handle(url: url)
+            }
+    }
 
 }
 
